eb2615533fca6ea58f0a9d8d3213409f4bd85c5c
cynthia <cchelangat9892gmail.com>
2022-09-27 16:04:32 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({links, bio}) {
+  function handleBio() {
+    if(!bio || bio === "") {
+      return null
+    }else{
+      return <p clasName="bio">{bio}</p>
+    }
+  }
+  // condition ? returnIfTrue : returnIfFalse
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {handleBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin}/>
     </div>
   );
 }
